742040b0bd8e91d61e23101a4b051045676a2f8f
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2024-06-26 09:41:14 +0000

    data Types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=1234
+let accountEmail="shubh24@gmail.com"
+var accountPassword="54321"
+accountCity="jaipur"
+
+/* 
+let ka use krenge var ka use nhi krenge kyuki usse block scope or functional scope me issue aayega
+*/
+
+//accountId="876" not allowed to change it 
+let accountState
+accountEmail="s34@hc.com"
+accountPassword="po5657"
+accountCity="madhubani"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js  code as newer version
+//alert(3+3)  we are using node js , not browser; 
+/* 
+ data types :-  
+ premitive data types =[ number     bigint   string     boolean- true/false   null--(standalone value)  undefined    symbol(used for uniqueness )   ]
+ 
+
+ object
+*/
+let state
+let name="hitesh"
+let age = 18
+let isLoggedIn=false
+
+
+console.log(typeof undefined); // undefined aayega isme 
+console.log(typeof null);   // iska type object aayega 
