35d67da4ac4d60c844a7a87e9d990278d593eedf
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2024-06-26 10:55:29 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,6 @@
+// console.log(null > 0); // false
+// console.log(null == 0);  // false
+
+// console.log(null >= 0);   // true   ( an equity cheak and comparison works diffrently   comparison convert null to a number and treating it to zero thats why null> 0  is false and null >= 0 is true   )
+
+// === ye strictly cheak krega inke data type ko bhi so "2" === 2 is false because one is string and other is number 
