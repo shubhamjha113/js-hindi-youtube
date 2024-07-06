333cc5ab100be4f9c65e5c0fdee849dabec94d33
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2024-07-06 03:51:17 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"       yaha agar var use krte to wo scope se bahar bhi value de deta 
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");    isko implicit scope bolenge mtlb man lia h hmne ki scope h but is trah comma lgake likhna achaa bat nhi h 
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {                       isko nested loop bolte h 
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,17 @@
+
+const month = 3      // agar yaha month = "april"  rhta to ye string hua to isko us hisab se compare krenge jaise case "april"
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default: 
+        console.log("none of these ");
+        break;
+}
