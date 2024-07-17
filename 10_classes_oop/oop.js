fc38c8a6952971b37b1dc88f338a6e374f29f78f
Shubham Kumar Jha <shubhamkumarjha564@gmail.com>
2024-07-17 07:44:01 +0000

    classes obj and prototype

---
+++ 10_classes_oop/oop.js
@@ -0,0 +1,41 @@
+
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+// new keyword constructor function h 
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)   // new keyword use nhi krenge to value overwrite kr dega or userOne print krenge to userTwo ka property dega sb 
+console.log(userOne.constructor);    // constructor ka mtlb basically khud ke bare me reference 
+//console.log(userTwo);
